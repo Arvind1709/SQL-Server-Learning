0f29143f0c0cb9288ce1d920920bf5ca9058b986
Arvind1709 <arvindyadav1709@gmail.com>
2026-07-04 23:11:35 +0530

    feat: add SELECT and column alias examples

--- 01_Basics/01_SELECT.sql
+++ 01_Basics/01_SELECT.sql
@@ -3,3 +3,18 @@
 select * from production.brands;
 select * from [production].[categories];
 select * from [sales].[customers];
+
+SELECT
+    brand_id AS Id,
+    brand_name AS Name
+FROM production.brands;
+
+SELECT
+    brand_name AS [Bike Brand]
+FROM production.brands;
+
+SELECT
+    brand_id,
+    brand_name AS Brand,
+    'Available' AS Status
+FROM production.brands;
